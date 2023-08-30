c9f3d1a8f029dbabbf2b684ff6f4472bc36e0559
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-30 15:09:52 +0530

    Added file

---
+++ C++/All New CodeChef.cpp
@@ -0,0 +1,16 @@
+ // August Long 2023 Division 4 Contest
+#include <iostream>
+using namespace std;
+
+int main() {
+	int time1;
+	cin >> time2;
+	int time2;
+	cin >> time2;
+  
+	if (time2 < time1) cout << "New";
+	else if (time2 > time1) cout << "Old";
+	else cout << "Same";
+  
+	return 0;
+}
