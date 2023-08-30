7e27064cfdc0da671b0734c9d9f70167c2633500
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-08-30 15:03:11 +0530

    Added file

---
+++ C++/I Can Use Both Hands!.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// Time complexity: O(1), Space complexity: O(1)
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int page, code, min;
+        cin >> page >> code >> min;
+        int ans = 0;
+        if(min % page == 0){
+            ans = ans + min / page;
+        }
+        else if(min % page != 0){
+            ans = ans + min / page + 1;
+        }
+        ans = ans + min / code;
+        cout << ans << endl;
+    }
+	return 0;
+}
