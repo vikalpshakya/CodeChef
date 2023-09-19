103e89d8aba31fe00261dbcb4b47b575ca68933d
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-20 00:45:00 +0530

    Added file

---
+++ C++/Chocolate Distribution.cpp
@@ -0,0 +1,14 @@
+// START 100D CONTEST
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    int n;
+	    cin>>n;
+	    cout << 1 <<" "<< 1 <<" " << n-2<<endl;
+	}
+	return 0;
+}
