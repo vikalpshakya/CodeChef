67a4239bcbbce18672b9922b6c6e051bbb09142c
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-21 02:40:20 +0530

    Added file

---
+++ C++/Off By One.cpp
@@ -0,0 +1,12 @@
+// Starters 101 Division 4 (Rated)  
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	string ans = "";
+	string c = to_string(a+b);
+	cout << stoi( ans+c+"1");
+	return 0;
+}
