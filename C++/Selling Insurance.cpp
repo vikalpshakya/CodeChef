dc31c656c5f960f1ac93e5eb0bf0f0cb8e192f98
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-27 23:37:02 +0530

    Added file

---
+++ C++/Selling Insurance.cpp
@@ -0,0 +1,18 @@
+ // START102D  contest
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    int n; 
+	    cin >> n;
+	    int a;
+	    a = ceil(100/(n*0.2));
+	    cout << a << endl;
+	}
+	return 0;
+}
