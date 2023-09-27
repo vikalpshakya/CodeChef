4adb1f2a73422ce36bd17f2cf8e0dce461eda67d
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-09-27 23:36:23 +0530

    Added file

---
+++ C++/Sports Section.cpp
@@ -0,0 +1,15 @@
+ // START102D  contest
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if(n>=8){
+	    cout << "YES" << endl;
+	}
+	else 
+	cout << "NO" << endl;
+	return 0;
+}
