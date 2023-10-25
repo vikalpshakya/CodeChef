62933e612293f59706c40eb1e89009c7ef89b8e1
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-10-25 22:05:19 +0530

    Added file

---
+++ C++/Dull Operation.cpp
@@ -0,0 +1,21 @@
+// Starters 105
+
+#include <iostream>
+using namespace std;
+#define ll long long
+
+
+int main() {
+	int tt;
+	cin >> tt;
+	while(tt--){
+	    ll n;
+	    cin >> n;
+	    cout << n << " " << n-1 << endl;
+	    
+	}
+	return 0;
+}
+
+
+
