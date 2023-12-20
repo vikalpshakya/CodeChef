dea1e88036db9e4ece523742c827908722ef13ac
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-12-20 23:57:01 +0530

    Added file

---
+++ C++/Maximum Score.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define ll long long
+#define int long long
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define input(x) for(auto &a:x) cin >> a;
+#define output(x) for(auto a:x) cout << a <<" ";
+#define FOR(i,n) for(i=0;i<n;i++)
+using namespace std; 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        
+        int n; cin >> n;
+        vi vec(n);
+        int i = 0,c1 = 0,c2=0;
+        FOR(i,n){ cin >> vec[i];
+            if(vec[i] == 0) c1++;
+            else c2++;
+                }
+        cout << min(c1,c2) << nL;
+
+    }
+    return 0;
+}
