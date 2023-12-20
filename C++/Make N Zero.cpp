f15e45b3488328e5cee28e8d27022c6dafb07fd5
Vikalp Shakya <118770374+vikalpshakya@users.noreply.github.com>
2023-12-20 23:58:23 +0530

    Added file

---
+++ C++/Make N Zero.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define ll long long
+#define int long long
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define input(x) for(auto &a:x) cin >> a;
+#define output(x) for(auto a:x) cout << a <<" ";
+#define FOR(i,n) for(i=0;i<n;i++)
+using namespace std; 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) {
+        
+        int n; cin >> n;
+        if(n >= 6 || n == 3 || n == 4){
+            cout << "YES" << nL;
+        }
+        else cout << "NO\n";
+   
+    }
+    return 0;
+}
